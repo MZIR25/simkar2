72e3be843ffc35d8619be975f8c8338e09d057ce
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2022-11-19 11:05:16 +0800

    add macro banner to redirect

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +15,12 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        RedirectResponse::macro('banner', function ($message, $style = 'success') {
+            return ((object) $this)->with([
+                'bannerStyle' => $style,
+                'banner' => $message,
+            ]);
+        });
     }
 
     /**
