dcb4d8207e241e1835cb557258eec63645f120d7
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2022-12-03 13:34:09 +0800

    fix id dependency

--- tests/Feature/Iterasi8Test.php
+++ tests/Feature/Iterasi8Test.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\Devisi;
+use App\Models\Jabatan;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -10,7 +12,11 @@ use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use App\Models\User;
+use Database\Factories\UserFactory;
 use Illuminate\Auth\Events\Authenticated;
+
+use function PHPUnit\Framework\assertNull;
+use function PHPUnit\Framework\assertSame;
 
 class Iterasi8Test extends TestCase
 {
@@ -39,7 +45,7 @@ class Iterasi8Test extends TestCase
     {
         $user = User::where('level','admin')->first();
         $response = $this->actingAs($user)
-            ->put(route('update_jabatan', '7'), [
+            ->put(route('update_jabatan', Jabatan::firstOrFail()->jabatan_id), [
                 'Nama_Jabatan' =>  $this->faker->jobTitle,
                 ]);
                 $this->withoutExceptionHandling();
@@ -49,8 +55,11 @@ class Iterasi8Test extends TestCase
     public function testHapusDaftarJabatan()
     {
         $user = User::where('level','admin')->first();
-        $response = $this->actingAs($user)->delete(route('delete_jabatan','12'));
+        $tesDelete = Jabatan::firstOrFail();
+        $response = $this->actingAs($user)->delete(route('delete_jabatan', $tesDelete->jabatan_id));
         $response->assertStatus(302);
+
+        assertNull(Jabatan::find($tesDelete->jabatan_id));
     }
 
     public function testLihatDaftarDevisi()
@@ -74,19 +83,27 @@ class Iterasi8Test extends TestCase
 public function testEditDaftarDevisi()
 {
     $user = User::where('level','admin')->first();
+    $id = Devisi::firstOrFail()->devisi_id;
+    $newTitle = $this->faker->jobTitle;
+
     $response = $this->actingAs($user)
-        ->put(route('update_devisi', '5'), [
-            'Nama_Devisi' =>  $this->faker->jobTitle,
+        ->put(route('update_devisi', $id), [
+            'Nama_Devisi' =>  $newTitle,
             ]);
             $this->withoutExceptionHandling();
-        // dd($response);
+
          $response->assertStatus(302);
+
+    assertSame($newTitle, Devisi::find($id)->Nama_Devisi);
 }
 public function testHapusDaftarDevisi()
 {
     $user = User::where('level','admin')->first();
-    $response = $this->actingAs($user)->delete(route('delete_devisi','7'));
+    $id = Devisi::firstOrFail()->devisi_id;
+    $response = $this->actingAs($user)->delete(route('delete_devisi',$id));
     $response->assertStatus(302);
+
+    assertNull(Devisi::find($id));
 }
 
 }
