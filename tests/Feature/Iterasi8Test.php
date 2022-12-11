b432cc38bbb244a8778f0207aca86b5114c2ae1d
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2022-12-11 11:43:16 +0800

    fix Iterasi8Test

--- tests/Feature/Iterasi8Test.php
+++ tests/Feature/Iterasi8Test.php
@@ -18,91 +18,91 @@ use Illuminate\Auth\Events\Authenticated;
 use function PHPUnit\Framework\assertNull;
 use function PHPUnit\Framework\assertSame;
 
-// class Iterasi8Test extends TestCase
-// {
-//     use WithoutMiddleware;
-//     use WithFaker;
+class Iterasi8Test extends TestCase
+{
+    use WithoutMiddleware;
+    use WithFaker;
 
-//     public function testLihatDaftarJabatan()
-//     {
-//         $user = User::where('level', 'admin')->first();
-//         $response = $this->actingAs($user)
-//             ->get(route('daftar_jabatan'));
-//         $response->assertStatus(200);
-//     }
-//     public function testTambahDaftarJabatan()
-//     {
-//         $user = User::where('level', 'admin')->first();
-//         $response = $this->actingAs($user)
-//             ->post(route('simpan_jabatan'), [
-//                 'Nama_Jabatan' =>  $this->faker->jobTitle,
-//             ]);
-//         $this->withoutExceptionHandling();
-//         // dd($response);
-//         $response->assertStatus(302);
-//     }
-//     public function testEditDaftarJabatan()
-//     {
-//         $user = User::where('level', 'admin')->first();
-//         $response = $this->actingAs($user)
-//             ->put(route('update_jabatan', Jabatan::firstOrFail()->jabatan_id), [
-//                 'Nama_Jabatan' =>  $this->faker->jobTitle,
-//             ]);
-//         $this->withoutExceptionHandling();
-//         // dd($response);
-//         $response->assertStatus(302);
-//     }
-//     public function testHapusDaftarJabatan()
-//     {
-//         $user = User::where('level', 'admin')->first();
-//         $tesDelete = Jabatan::firstOrFail();
-//         $response = $this->actingAs($user)->delete(route('delete_jabatan', $tesDelete->jabatan_id));
-//         $response->assertStatus(302);
+    public function testLihatDaftarJabatan()
+    {
+        $user = User::where('level', 'admin')->first();
+        $response = $this->actingAs($user)
+            ->get(route('daftar_jabatan'));
+        $response->assertStatus(200);
+    }
+    public function testTambahDaftarJabatan()
+    {
+        $user = User::where('level', 'admin')->first();
+        $response = $this->actingAs($user)
+            ->post(route('simpan_jabatan'), [
+                'Nama_Jabatan' =>  $this->faker->jobTitle,
+            ]);
+        $this->withoutExceptionHandling();
+        // dd($response);
+        $response->assertStatus(302);
+    }
+    public function testEditDaftarJabatan()
+    {
+        $user = User::where('level', 'admin')->first();
+        $response = $this->actingAs($user)
+            ->put(route('update_jabatan', Jabatan::firstOrFail()->jabatan_id), [
+                'Nama_Jabatan' =>  $this->faker->jobTitle,
+            ]);
+        $this->withoutExceptionHandling();
+        // dd($response);
+        $response->assertStatus(302);
+    }
+    public function testHapusDaftarJabatan()
+    {
+        $user = User::where('level', 'admin')->first();
+        $tesDelete = Jabatan::firstOrFail();
+        $response = $this->actingAs($user)->delete(route('delete_jabatan', $tesDelete->jabatan_id));
+        $response->assertStatus(302);
 
-//         assertNull(Jabatan::find($tesDelete->jabatan_id));
-//     }
+        assertNull(Jabatan::find($tesDelete->jabatan_id));
+    }
 
-//     public function testLihatDaftarDevisi()
-//     {
-//         $user = User::where('level', 'admin')->first();
-//         $response = $this->actingAs($user)
-//             ->get(route('daftar_jabatan'));
-//         $response->assertStatus(200);
-//     }
-//     public function testTambahDaftarDevisi()
-//     {
-//         $user = User::where('level', 'admin')->first();
-//         $response = $this->actingAs($user)
-//             ->post(route('simpan_devisi'), [
-//                 'Nama_Devisi' =>  $this->faker->jobTitle,
-//             ]);
-//         $this->withoutExceptionHandling();
-//         // dd($response);
-//         $response->assertStatus(302);
-//     }
-//     public function testEditDaftarDevisi()
-//     {
-//         $user = User::where('level', 'admin')->first();
-//         $id = Devisi::firstOrFail()->devisi_id;
-//         $newTitle = $this->faker->jobTitle;
+    public function testLihatDaftarDevisi()
+    {
+        $user = User::where('level', 'admin')->first();
+        $response = $this->actingAs($user)
+            ->get(route('daftar_jabatan'));
+        $response->assertStatus(200);
+    }
+    public function testTambahDaftarDevisi()
+    {
+        $user = User::where('level', 'admin')->first();
+        $response = $this->actingAs($user)
+            ->post(route('simpan_devisi'), [
+                'Nama_Devisi' =>  $this->faker->jobTitle,
+            ]);
+        $this->withoutExceptionHandling();
+        // dd($response);
+        $response->assertStatus(302);
+    }
+    public function testEditDaftarDevisi()
+    {
+        $user = User::where('level', 'admin')->first();
+        $id = Devisi::firstOrFail()->devisi_id;
+        $newTitle = $this->faker->jobTitle;
 
-//         $response = $this->actingAs($user)
-//             ->put(route('update_devisi', $id), [
-//                 'Nama_Devisi' =>  $newTitle,
-//             ]);
-//         $this->withoutExceptionHandling();
+        $response = $this->actingAs($user)
+            ->put(route('update_devisi', $id), [
+                'Nama_Devisi' =>  $newTitle,
+            ]);
+        $this->withoutExceptionHandling();
 
-//         $response->assertStatus(302);
+        $response->assertStatus(302);
 
-//         assertSame($newTitle, Devisi::find($id)->Nama_Devisi);
-//     }
-//     public function testHapusDaftarDevisi()
-//     {
-//         $user = User::where('level', 'admin')->first();
-//         $id = Devisi::firstOrFail()->devisi_id;
-//         $response = $this->actingAs($user)->delete(route('delete_devisi', $id));
-//         $response->assertStatus(302);
+        assertSame($newTitle, Devisi::find($id)->Nama_Devisi);
+    }
+    public function testHapusDaftarDevisi()
+    {
+        $user = User::where('level', 'admin')->first();
+        $id = Devisi::firstOrFail()->devisi_id;
+        $response = $this->actingAs($user)->delete(route('delete_devisi', $id));
+        $response->assertStatus(302);
 
-//         assertNull(Devisi::find($id));
-//     }
-// }
+        assertNull(Devisi::find($id));
+    }
+}
