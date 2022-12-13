ab4f9d1866d4a3a62a520a4f625941c2d2dd19e4
MZIR25 <muhikram1609@gmail.com>
2022-12-13 17:05:19 +0800

    pdf

--- resources/views/Presensi/v_rekap_presensi.blade.php
+++ resources/views/Presensi/v_rekap_presensi.blade.php
@@ -1,13 +1,50 @@
-@extends('layouts.index')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>SIMKAN ValTech</title>
 
-@section('content')
+  <script src="{{ mix('js/app.js') }}"></script>
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  {{-- <link rel="stylesheet" href="{{asset('fontawesome-free/')}}/css/all.min.css"> --}}
+  <link rel="stylesheet" href="{{asset('template/')}}/plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet" href="{{asset('template/')}}/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="{{asset('template/')}}/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="{{asset('template/')}}/plugins/jqvmap/jqvmap.min.css">
+  <!-- Theme style -->
+  {{-- <link rel="stylesheet" href="{{asset('dist/')}}/css/adminlte.min.css"> --}}
+  <link rel="stylesheet" href="{{asset('template/')}}/dist/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="{{asset('template/')}}/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="{{asset('template/')}}/plugins/daterangepicker/daterangepicker.css">
+  <!-- summernote -->
+  <link rel="stylesheet" href="{{asset('template/')}}/plugins/summernote/summernote-bs4.min.css">
+  <link rel="stylesheet" href="{{asset('template/')}}/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="{{asset('template/')}}/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="{{asset('template/')}}/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+  <link rel="stylesheet" href="{{asset('template/plugins/toastr/toastr.min.css')}}">
+    <!-- jQuery -->
+    <script src="{{asset('template/')}}/plugins/jquery/jquery.min.js"></script>
+    <!-- jQuery UI 1.11.4 -->
+    <script src="{{asset('template/')}}/plugins/jquery-ui/jquery-ui.min.js"></script>
+</head>
 <div class="row">
     <div class="col-md-12 p-5 pt-2">
         <div class="card">
             <div class="card-body">
                 <h3><i class="fas fa-swatchbook"></i></i>Daftar Rekap Presensi</h3><hr>
-                    <a href="" class="btn btn-primary mb-3"><i class="fas fa-plus-square mr-2"></i>Rekap Presensi</a>
-                    <table id="myTable" class="table table-striped table-bordered" >
+                    {{-- <a href="" class="btn btn-primary mb-3"><i class="fas fa-plus-square mr-2"></i>Rekap Presensi</a> --}}
+                    <table class="table table-striped table-bordered" >
                         <thead>
                         <tr>
                             <th scope="col">No.</th>
@@ -15,52 +52,71 @@
                             <th scope="col">Tanggal</th>
                             <th scope="col">Bulan</th>
                             <th scope="col">Status</th>
-                            <th style="width: 8%">Aksi</th>
+
                         </tr>
                     </thead>
                     <tbody>
-
                         <tr>
                             <td></td>
                             <td></td>
                             <td></td>
                             <td></td>
                             <td></td>
-                            <td>
-                                <div class="row">
-                                    <a href=""><i class="fas fa-eye bg-success p-2 text-white rounded ml-2 mr-1" data-toggle="tooltip" title="Edit"></i></a>
-                                </div>
-                            </td>
-                        </tr>
 
+                        </tr>
                     </tbody>
                 </table>
-
             </div>
         </div>
     </div>
 </div>
-@if (auth()->user()->level == "admin")
+<script src="{{asset('template/')}}/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="{{asset('template/')}}/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="{{asset('template/')}}/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="{{asset('template/')}}/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="{{asset('template/')}}/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="{{asset('template/')}}/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="{{asset('template/')}}/plugins/jszip/jszip.min.js"></script>
+<script src="{{asset('template/')}}/plugins/pdfmake/pdfmake.min.js"></script>
+<script src="{{asset('template/')}}/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="{{asset('template/')}}/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="{{asset('template/')}}/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="{{asset('template/')}}/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<!-- Toast -->
+<script src="{{asset('template/plugins/toastr/toastr.min.js')}}"></script>
 <script>
+    $.widget.bridge('uibutton', $.ui.button)
+  </script>
+  <!-- Bootstrap 4 -->
+  <script src="{{asset('template/')}}/plugins/bootstrap/dist/js.bundle.min.js"></script>
+  <!-- ChartJS -->
+  <script src="{{asset('template/')}}/plugins/chart.js/Chart.min.js"></script>
+  <!-- Sparkline -->
+  <script src="{{asset('template/')}}/plugins/sparklines/sparkline.js"></script>
+  <!-- JQVMap -->
+  <script src="{{asset('template/')}}/plugins/jqvmap/jquery.vmap.min.js"></script>
+  <script src="{{asset('template/')}}/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+  <!-- jQuery Knob Chart -->
+  <script src="{{asset('template/')}}/plugins/jquery-knob/jquery.knob.min.js"></script>
+  <!-- daterangepicker -->
+  <script src="{{asset('template/')}}/plugins/moment/moment.min.js"></script>
+  <script src="{{asset('template/')}}/plugins/daterangepicker/daterangepicker.js"></script>
+  <!-- Tempusdominus Bootstrap 4 -->
+  <script src="{{asset('template/')}}/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+  <!-- Summernote -->
+  <script src="{{asset('template/')}}/plugins/summernote/summernote-bs4.min.js"></script>
+  <!-- overlayScrollbars -->
+  <script src="{{asset('template/')}}/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+  <script>
     $(function () {
      $("#myTable").DataTable({
 
             "responsive": true,
-            "lengthChange": true,
+            "lengthChange": false,
             "autoWidth": false,
-            "searching": true,
-            "buttons": false,
+            "searching": false,
+            "paging": false,
+            "ordering": false,
          }).buttons().remove().container().appendTo('#myTable_wrapper .fluid col-md-6:eq(0)');
-         // $('#example2').DataTable({
-         //     "paging": true,
-         //     "lengthChange": false,
-         //     "searching": false,
-         //     "ordering": true,
-         //     "info": true,
-         //     "autoWidth": false,
-         //     "responsive": true,
-         //   });
-     });
+        });
  </script>
-@endif
-@endsection
